964d29f0bb084d998ecad122cec935b9142dc874
YMyajima <buiiudymi.yajima@gmail.com>
2019-08-03 11:38:11 +0900

    solved lesson 2-2

--- src/com/codility/lesson2_2/Lesson2_2.java
+++ src/com/codility/lesson2_2/Lesson2_2.java
@@ -1,13 +1,35 @@
 package com.codility.lesson2_2;
 
+import sun.jvm.hotspot.utilities.Assert;
+
 import java.util.Arrays;
 
 public class Lesson2_2 {
 
+    enum Test {
+        a(new int[]{3, 8, 9, 7, 6}, 3, new int[]{9, 7, 6, 3, 8}),
+        b(new int[]{0, 0, 0}, 3, new int[]{0, 0, 0}),
+        c(new int[]{1, 2, 3}, 2, new int[]{2, 3, 1}),
+        d(new int[]{}, 0, new int[]{}),
+
+        ;
+
+        int[] A;
+        int K;
+        int[] expect;
+
+        Test(int[] a, int k, int[] expect) {
+            A = a;
+            K = k;
+            this.expect = expect;
+        }
+    }
+
     public static void main(String[] args) {
-        int[] A = {3, 8, 9, 7, 6};
-        int K = 3;
-        int[] result = new Solution().solution(A, K);
-        System.out.println(Arrays.toString(result));
+        Test[] values = Test.values();
+        for (Test test : values) {
+            int[] result = new Solution().solution(test.A, test.K);
+            Assert.that(Arrays.equals(test.expect, result), "");
+        }
     }
 }
--- src/com/codility/lesson2_2/Solution.java
+++ src/com/codility/lesson2_2/Solution.java
@@ -1,8 +1,32 @@
 package com.codility.lesson2_2;
 
+import java.util.Arrays;
+
 class Solution {
+    private int nextIndex(int aLength, int rotateCount, int targetIndex) {
+        int tmp = targetIndex + rotateCount;
+        int nextIndex = tmp % aLength;
+        return nextIndex;
+    }
+
     public int[] solution(int[] A, int K) {
         // write your code in Java SE 8
-        return A;
+        System.out.println(Arrays.toString(A));
+        int L = A.length;
+        if (L == 0) {
+            return A;
+        }
+        int rotateCount = K % L;
+        if (rotateCount == 0) {
+            return A;
+        }
+        int[] result = new int[A.length];
+        int aLength = A.length;
+        for (int i = 0; i < aLength; i++) {
+            int nextIndex = nextIndex(aLength, rotateCount, i);
+            result[nextIndex] = A[i];
+        }
+        System.out.println(Arrays.toString(result));
+        return result;
     }
 }
